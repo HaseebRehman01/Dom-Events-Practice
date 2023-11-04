9690a4a3e0975d606e43f70a4a2816e3ce6f7aef
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-11-05 02:30:54 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var btn = document.querySelector(".btn")
+// one way 
+
+// function click(){
+// alert("hello")
+// }
+
+// btn.onmouseenter = click
+
+//second way
+// btn.onclick = function(){
+//     alert("hello")
+// }
+
+//Third way 
+// addEventListener
+
+
+// fourth way
+// first write onclick="func()" in html then write onclick work inside func() in JS
+
+//  eventlistener
+// btn.addEventListener("mouseenter",function(){
+//     alert("I learned eventlistener")
+// })
